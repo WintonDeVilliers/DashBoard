fb4e1f4d7dfd811af73945307b4d894c356db026
Winton <65846344+WintonDeVilliers@users.noreply.github.com>
2025-08-19 10:31:16 +0000

    latest

--- f1-dashboard-nextjs-extract/hooks/index.js
+++
@@ -1,3 +0,0 @@
-// F1 Racing Dashboard - Hooks Exports
-export { useIsMobile } from './use-mobile';
-export { useToast } from './use-toast';
--- f1-dashboard-nextjs-extract/utils/index.js
+++
@@ -1,4 +0,0 @@
-// F1 Racing Dashboard - Utility Exports
-export { processExcelData, ExcelProcessor } from './excelProcessor';
-export { PerformanceCalculator } from './performanceCalculations';
-export * from './racingAnimations';
